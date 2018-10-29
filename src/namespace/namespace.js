bd7689a495d3e679babd9d99aa4f28cfa41e111c
Loredana <loredana.cirstea@gmail.com>
2018-10-29 14:51:45 +0100

    Add PipeProxy testnet addresses

--- src/namespace/namespace.js
+++ src/namespace/namespace.js
@@ -13,6 +13,13 @@ const Pipeos = {
             tag: `${pipeserverIp}/tag`,
         },
     },
+    contracts: {
+        PipeProxy: {
+            '3': '0x97dc6bbd4f1cd2bd24e341bf01ca9b8bb433da46',
+            '4': '0x07ed1484f787e702a50aaef193ed1c6f5bd677cb',
+            '42': '0x7f4a8db89e96110a48f299043084c69aa190c58c',
+        },
+    },
     remix: new RemixExtension(),
 };
 
