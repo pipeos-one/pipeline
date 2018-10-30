fbf6011231747fb24cee706ac74d6eb5c834a9f7
Loredana <loredana.cirstea@gmail.com>
2018-10-30 09:44:14 +0100

    Add tooltips in PipeTree

--- src/components/PipeTree.vue
+++ src/components/PipeTree.vue
@@ -11,14 +11,27 @@
             :key="item._id"
             v-bind:class="[item.styleClasses]"
           >
-            <v-btn
-                block
-                flat
-                v-on:click="$emit('item-toggle', item)"
-                v-bind:class="[item.styleClasses, 'normaltxt']"
-            >
-                {{ item.abiObj.name }}
-            </v-btn>
+            <v-tooltip right>
+                <v-btn
+                    block
+                    flat
+                    slot="activator"
+                    v-on:click="$emit('item-toggle', item)"
+                    v-bind:class="[item.styleClasses, 'normaltxt']"
+                >
+                    {{ item.abiObj.name }}
+                </v-btn>
+                <p class="text-sm-left" v-if="item.userdoc">{{item.userdoc.notice}}</p>
+                <template
+                    v-if="item.devdoc"
+                >
+                    <template
+                        v-for="(param, index) in Object.keys(item.devdoc.params)">
+                        <p class="text-sm-left">{{param}}: {{item.devdoc.params[param]}}</p>
+                    </template>
+                    <p class="text-sm-left" v-if="item.devdoc.return">Returns: {{item.devdoc.return}}</p>
+                </template>
+              </v-tooltip>
           </v-subheader>
         </template>
       </v-list>
