18530e881d28024342dd6db2b0c2a9c066242d73
Loredana <loredana.cirstea@gmail.com>
2018-10-31 10:37:30 +0100

    Button fix for PipeTree

--- src/components/PipeTree.vue
+++ src/components/PipeTree.vue
@@ -7,32 +7,32 @@
       <div slot="header">{{item.name}}</div>
       <v-list two-line>
         <template v-for="(item, index) in item.functions" v-if="item.abiObj.name">
-          <v-subheader
-            :key="item._id"
-            v-bind:class="[item.styleClasses]"
-          >
             <v-tooltip right>
+              <v-subheader
+                :key="item._id"
+                v-bind:class="[item.styleClasses]"
+                slot="activator"
+              >
                 <v-btn
                     block
                     flat
-                    slot="activator"
                     v-on:click="$emit('item-toggle', item)"
                     v-bind:class="[item.styleClasses, 'normaltxt']"
                 >
                     {{ item.abiObj.name }}
                 </v-btn>
-                <p class="text-sm-left" v-if="item.userdoc">{{item.userdoc.notice}}</p>
-                <template
-                    v-if="item.devdoc"
-                >
-                    <template
-                        v-for="(param, index) in Object.keys(item.devdoc.params)">
-                        <p class="text-sm-left">{{param}}: {{item.devdoc.params[param]}}</p>
-                    </template>
-                    <p class="text-sm-left" v-if="item.devdoc.return">Returns: {{item.devdoc.return}}</p>
-                </template>
-              </v-tooltip>
-          </v-subheader>
+              </v-subheader>
+              <p class="text-sm-left" v-if="item.userdoc">{{item.userdoc.notice}}</p>
+              <template
+                  v-if="item.devdoc"
+              >
+                  <template
+                      v-for="(param, index) in Object.keys(item.devdoc.params)">
+                      <p class="text-sm-left">{{param}}: {{item.devdoc.params[param]}}</p>
+                  </template>
+                  <p class="text-sm-left" v-if="item.devdoc.return">Returns: {{item.devdoc.return}}</p>
+              </template>
+            </v-tooltip>
         </template>
       </v-list>
     </v-expansion-panel-content>
