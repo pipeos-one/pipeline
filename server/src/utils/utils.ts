a95e998a01e2c4cdf64392e26447b5443a6654fd
Loredana <loredana.cirstea@gmail.com>
2019-02-18 22:59:20 +0200

    Add range function utility

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -1,6 +1,11 @@
-export let reverseKeys = (data: any) => {
+export const reverseKeys = (data: any) => {
     return Object.keys(data).reduce(function(obj: any, key: any) {
        obj[data[key]] = key;
        return obj;
     },{});
+}
+
+export const range = (start: number=0, end: number, step: number=1) => {
+    const length: number = end - start;
+    return Array.from({ length }, (_, i) => i * step + start);
 }
