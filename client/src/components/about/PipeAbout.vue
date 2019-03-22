a137b613f3b49c7356a6a0ff4b479cf0dbcea7b5
Loredana <loredana.cirstea@gmail.com>
2019-03-22 17:49:32 +0200

    Update home page and logos

--- client/src/components/about/PipeAbout.vue
+++ client/src/components/about/PipeAbout.vue
@@ -2,63 +2,65 @@
     <div>
         <v-toolbar flat color="#fff" floating>
             <v-btn
-                small flat fab
-                href="https://github.com/pipeos-one/pipeos-client"
+                flat block
+                href="https://github.com/pipeos-one/pipeline"
                 target="_blank"
             >
-                <v-icon>fa-code-branch</v-icon>
+                github
             </v-btn>
             <v-btn
-                small flat fab
+                flat block
                 href="https://gitter.im/pipeos-one/pipeline"
                 target="_blank"
             >
-                <v-icon>fa-comments</v-icon>
+                gitter
             </v-btn>
-
+            <v-btn
+                flat block
+                href="https://www.youtube.com/watch?v=kOhmv9uj1mI&list=PL323JufuD9JAyaYCEvrnQ5yOAc3ukuNyF&index=9&t=1s"
+                target="_blank"
+            >
+                youtube
+            </v-btn>
         </v-toolbar>
-
-<div>
-
-
-            <v-chip class="display-1">
-              <v-avatar>
-
-                <v-img
-          :src="`/pipeline.png`"
-          aspect-ratio="1"
-          class="grey lighten-2"
-          height="44"
-          width="44"
-        ></v-img>
-              </v-avatar>
-              Pipeline: The Visual IDE.(WIP)
-            </v-chip>
-
-
-    <div><br>
-    If you know this video: maximize the window: <img src="/maximize.png"
-          height="32"
-          width="32"
-        ></img> and click next: <img
-          src="/next.png"
-          height="33"
-          width="33"
-        ></img>
-
-    <br><br>
-
-    </div>
-
         <div>
-
-        <iframe width="476" height="280" src="https://www.youtube.com/embed/kOhmv9uj1mI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-
-
-
+            <div class="display-1">
+                <v-avatar>
+                    <v-img
+                        :src="`/pipeline.png`"
+                        aspect-ratio="1"
+                        class="grey lighten-2"
+                        height="44"
+                        width="44"
+                    ></v-img>
+                </v-avatar>
+                Pipeline: The Visual IDE (WIP)
+            </div>
+            <v-layout row wrap>
+                <v-flex xs12>
+                    <br>
+                    If you know this video: maximize the window:
+                    <img src="/maximize.png"
+                        height="32"
+                        width="32"
+                    ></img> and click next:
+                    <img
+                        src="/next.png"
+                        height="33"
+                        width="33"
+                    ></img>
+                </v-flex>
+                <v-flex xs12>
+                    <br>
+                    You can try out the old Pipeline with +4k contracts by loading <pre class="font-weight-bold">{"title":"Pipeline2","url":"https://pipeline-old.pipeos.one"}</pre> as a Remix plugin.
+                </v-flex>
+                <v-flex xs12>
+                    <br>
+                    <iframe width="476" height="280" src="https://www.youtube.com/embed/kOhmv9uj1mI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                </v-flex>
+            </v-layout>
         </div>
-
-    </div> </div>
+    </div>
 </template>
 
 <style>
