de51576959bcdb410c037f35d272b7b2bb197314
Loredana <loredana.cirstea@gmail.com>
2019-08-14 00:59:56 +0300

    @pipeos/pipecanvaslib move fixtures in a separate file

--- packages/packages/pipecanvas/src/pipecanvaslib.js
+++ packages/packages/pipecanvas/src/pipecanvaslib.js
@@ -2,10 +2,10 @@
 import SVG from 'svg.js';
 import 'svg.draggable.js';
 import './svg.foreignobject';
+import * as FIXTURES from './fixtures.js';
 
 const R = require('ramda');
 
-// import {contracts, functions, graphh} from './graphs.js';
 
 const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
 const ARGUMENT_NAMES = /([^\s,]+)/g;
@@ -23,183 +23,6 @@ function getParamNames(func) {
 
     return result;
 }
-
-const ports = [
-    {
-        _id: '5bc59e192817116e84bdd830',
-        pclassid: '5bc59d5d2817116e84bdd82e',
-        pclass: { name: 'PipeOS' },
-        pfunction: {
-            gapi: {
-                constant: true,
-                inputs: [
-                    {
-                        name: 'in',
-                        type: '*',
-                    },
-                ],
-                name: 'PortOut',
-                outputs: [],
-                payable: false,
-                stateMutability: 'view',
-                type: 'port',
-            },
-            signature: 'PortOut(*)',
-        },
-        timestamp: '2018-10-16T08:10:33.614Z',
-    },
-    {
-        _id: '5bc59e192817116e84bdd831',
-        pclassid: '5bc59d5d2817116e84bdd82e',
-        pclass: { name: 'PipeOS' },
-        pfunction: {
-            gapi: {
-                constant: true,
-                inputs: [],
-                name: 'PortIn',
-                outputs: [
-                    {
-                        name: 'out',
-                        type: '*',
-                    },
-                ],
-                payable: false,
-                stateMutability: 'view',
-                type: 'port',
-            },
-            signature: 'PortIn()',
-        },
-        timestamp: '2018-10-16T08:10:33.614Z',
-    },
-    {
-        _id: '5bc59e192817116e84bdd832',
-        pclassid: '5bc59d5d2817116e84bdd82e',
-        pclass: { name: 'PipeOS' },
-        pfunction: {
-            gapi: {
-                constant: true,
-                inputs: [],
-                name: 'MsgData',
-                outputs: [
-                    {
-                        name: 'data',
-                        type: 'bytes',
-                    },
-                    {
-                        name: 'sender',
-                        type: 'address',
-                    },
-                    {
-                        name: 'sig',
-                        type: 'bytes4',
-                    },
-                    {
-                        name: 'value',
-                        type: 'uint256',
-                    },
-                ],
-                payable: false,
-                stateMutability: 'view',
-                type: 'data',
-            },
-            signature: 'MsgData()',
-        },
-        timestamp: '2018-10-16T08:10:33.614Z',
-    },
-    {
-        _id: '5bc59e192817116e84bdd833',
-        pclassid: '5bc59d5d2817116e84bdd82e',
-        pclass: { name: 'PipeOS' },
-        pfunction: {
-            gapi: {
-                constant: true,
-                inputs: [],
-                name: 'BlockData',
-                outputs: [
-                    {
-                        name: 'coinbase',
-                        type: 'address',
-                    },
-                    {
-                        name: 'dificulty',
-                        type: 'uint256',
-                    },
-                    {
-                        name: 'gaslimit',
-                        type: 'uint256',
-                    },
-                    {
-                        name: 'number',
-                        type: 'uint256',
-                    },
-                    {
-                        name: 'timestamp',
-                        type: 'uint256',
-                    },
-                ],
-                payable: false,
-                stateMutability: 'view',
-                type: 'data',
-            },
-            signature: 'BlockData()',
-        },
-        timestamp: '2018-10-16T08:10:33.614Z',
-    },
-    {
-        _id: '5bc59e192817116e84bdd834',
-        pclassid: '5bc59d5d2817116e84bdd82e',
-        pclass: { name: 'PipeOS' },
-        pfunction: {
-            gapi: {
-                constant: true,
-                inputs: [],
-                name: 'MiscData',
-                outputs: [
-                    {
-                        name: 'gasleft',
-                        type: 'uint256',
-                    },
-                    {
-                        name: 'now',
-                        type: 'uint256',
-                    },
-                    {
-                        name: 'tx.gasprice',
-                        type: 'uint256',
-                    },
-                    {
-                        name: 'tx.origin',
-                        type: 'address',
-                    },
-                ],
-                payable: false,
-                stateMutability: 'view',
-                type: 'data',
-            },
-            signature: 'MiscData()',
-        },
-        timestamp: '2018-10-16T08:10:33.614Z',
-    },
-
-];
-
-const containers = [
-    {
-        _id: '5bc59d5d2817116e84bdd82e',
-        name: 'PipeOS',
-        pclass: {
-            gapi: [{
-                constant: true, inputs: [{ name: 'in', type: '*' }], name: 'PortOut', outputs: [], payable: false, stateMutability: 'view', type: 'port',
-            }, {
-                constant: true, inputs: [], name: 'PortIn', outputs: [{ name: 'out', type: '*' }], payable: false, stateMutability: 'view', type: 'port',
-            }],
-            natspec: { methods: {}, title: 'PipeOS Utils.' },
-            bytecode: '',
-            flatsource: ' ',
-        },
-        timestamp: '2018-10-16T08:10:33.614Z',
-    },
-];
 
 let pipe2 = {};
 let grIndex = 0
@@ -223,8 +46,8 @@ export default class Graphs {
 
     constructor(functions, callbacks){
         // console.log("constr", functions)
-        pipe2.functions = functions.concat(ports.map(port => {
-            port.container = containers[0];
+        pipe2.functions = functions.concat(FIXTURES.ports.map(port => {
+            port.container = FIXTURES.containers[0];
             return port;
         }));
         pipe2.graphs = []
@@ -312,8 +135,8 @@ export default class Graphs {
 
 // Expects functions as an array of pfunction objects, each with a `container` key for the pclass.
 const loadAll = function loadAll(domids, functions, graphs) {
-    pipe2.functions = functions.concat(ports.map(port => {
-        port.container = containers[0];
+    pipe2.functions = functions.concat(FIXTURES.ports.map(port => {
+        port.container = FIXTURES.containers[0];
         return port;
     }));
     pipe2.graphs = graphs;
