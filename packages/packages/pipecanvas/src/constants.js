a5e07f6ca4a076c496b5a1a86952208aad494cf2
Loredana <loredana.cirstea@gmail.com>
2019-12-19 20:04:54 +0200

    Remove old pipecanvas source code

--- packages/packages/pipecanvas/src/constants.js
+++
@@ -1,6 +0,0 @@
-export const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-export const ARGUMENT_NAMES = /([^\s,]+)/g;
-export const UNNAMED_INPUT = 'i_unnamed';
-export const UNNAMED_OUTPUT = 'o_unnamed';
-export const WEI_VALUE = 'wei_value';
-export const DEPLOYMENT_VAR = 'deployment';
