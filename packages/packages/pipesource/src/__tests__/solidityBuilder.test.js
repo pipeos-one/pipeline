7d06d612523363f42518c93861b24bc4f2364718
Loredana <loredana.cirstea@gmail.com>
2019-12-25 02:45:19 +0200

    @pipeos/pipesource test sol + web3js with 1 event

--- packages/packages/pipesource/src/__tests__/solidityBuilder.test.js
+++ packages/packages/pipesource/src/__tests__/solidityBuilder.test.js
@@ -5,7 +5,17 @@ import graphFixt from './fixtures/enrichedGraph_fixtures';
 import solidityBuilderFixt from './fixtures/solidityBuilderSource_fixtures';
 
 test('web3Builder_vendorPriceMarketConnected', () => {
-  const web3Source = sourceBuilder(solidityBuilder)(graphFixt.vendorPriceMarketConnected)('function00');
+  const web3Source = sourceBuilder(solidityBuilder)
+    (graphFixt.vendorPriceMarketConnected)
+    ('function00');
 
   expect(web3Source.source).toBe(solidityBuilderFixt.vendorPriceMarketConnected);
+})
+
+test('web3Builder_priceMarketEvent1', () => {
+  const web3Source = sourceBuilder(solidityBuilder)
+    (graphFixt.priceMarketEvent1)
+    ('function00');
+
+  expect(web3Source.source).toBe(solidityBuilderFixt.priceMarketEvent1);
 })
