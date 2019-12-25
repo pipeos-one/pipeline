4837ff9ecbbdb437d0f8d2938f9214ba88e9146d
Loredana <loredana.cirstea@gmail.com>
2019-12-25 02:45:19 +0200

    @pipeos/pipesource test sol + web3js with 1 event

--- packages/packages/pipesource/src/__tests__/fixtures/solidityBuilderSource_fixtures.js
+++ packages/packages/pipesource/src/__tests__/fixtures/solidityBuilderSource_fixtures.js
@@ -38,4 +38,42 @@ contract PipedContract {
 
 }`;
 
-export default {vendorPriceMarketConnected};
+const priceMarketEvent1 = `pragma solidity ^0.5.4;
+pragma experimental ABIEncoderV2;
+
+interface MarketPlaceInterface {
+  function QuantitySet()
+    external nonpayable
+    returns (address, uint256, uint256);
+  function buy(address vendor, address buyer, uint256 product_id, uint256 quantity)
+    external payable;
+}
+interface VendorPricesInterface {
+  function calculateQuantity(uint256 product_id, address vendor, uint256 wei_value)
+    external view
+    returns (uint256);
+}
+
+
+contract PipedContract {
+  MarketPlaceInterface public MarketPlace;
+  VendorPricesInterface public VendorPrices;
+
+  constructor(address _MarketPlace_address, address _VendorPrices_address) public {
+    MarketPlace = MarketPlaceInterface(_MarketPlace_address);
+    VendorPrices = VendorPricesInterface(_VendorPrices_address);
+  }
+
+  function function00(address buyer_3000_0, uint256 wei_value_3002_0)
+    public payable
+    returns (uint256)
+  {
+    (address vendor_101_0, uint256 product_id_101_1, uint256 quantity_101_2) = MarketPlace.QuantitySet();
+    uint256 quantity_103_0 = VendorPrices.calculateQuantity(product_id_101_1, vendor_101_0, wei_value_3002_0);
+    MarketPlace.buy.value(wei_value_3002_0)(vendor_101_0, buyer_3000_0, product_id_101_1, quantity_103_0);
+    return quantity_101_2;
+  }
+
+}`;
+
+export default {vendorPriceMarketConnected, priceMarketEvent1};
--- packages/packages/pipesource/src/__tests__/fixtures/web3BuilderSource_fixtures.js
+++ packages/packages/pipesource/src/__tests__/fixtures/web3BuilderSource_fixtures.js
@@ -25,4 +25,35 @@ const vendorPriceMarketConnected = `function pipedGraph(address_VendorRegistrati
   return {function00};
 }`;
 
-export default {vendorPriceMarketConnected};
+const priceMarketEvent1 = `function pipedGraph(address_MarketPlace, address_VendorPrices, provider, signer, ethers) {
+
+
+  const abi_MarketPlace = [{"anonymous":false,"inputs":[],"name":"QuantitySet","type":"event","stateMutability":"nonpayable","outputs":[{"indexed":false,"name":"vendor","type":"address"},{"indexed":false,"name":"product_id","type":"uint256"},{"indexed":false,"name":"quantity","type":"uint256"}]},{"constant":false,"inputs":[{"name":"vendor","type":"address"},{"name":"buyer","type":"address"},{"name":"product_id","type":"uint256"},{"name":"quantity","type":"uint256"}],"name":"buy","outputs":[],"payable":true,"stateMutability":"payable","type":"function"}];
+  const MarketPlace = new ethers.Contract(address_MarketPlace, abi_MarketPlace, signer);
+
+  const abi_VendorPrices = [{"constant":true,"inputs":[{"name":"product_id","type":"uint256"},{"name":"vendor","type":"address"},{"name":"wei_value","type":"uint256"}],"name":"calculateQuantity","outputs":[{"name":"quantity","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
+  const VendorPrices = new ethers.Contract(address_VendorPrices, abi_VendorPrices, signer);
+
+  function function00(buyer_3000_0, wei_value_3002_0, callback)
+  {
+
+    const topics = [ethers.utils.id("QuantitySet(address,uint256,uint256)")];
+    const filter = {address: MarketPlace.address, topics};
+
+    provider.once(filter, async (result) => {
+      const {vendor_101_0, product_id_101_1, quantity_101_2} = ethers.utils.defaultAbiCoder.decode([{"name":"vendor_101_0","type":"address"},{"name":"product_id_101_1","type":"uint256"},{"indexed":false,"name":"quantity_101_2","type":"uint256"}], result.data);
+
+
+    const quantity_103_0 = await VendorPrices.calculateQuantity(product_id_101_1, vendor_101_0, wei_value_3002_0);
+
+    const done_102_0 = await MarketPlace.buy(vendor_101_0, buyer_3000_0, product_id_101_1, quantity_103_0, {value: wei_value_3002_0});
+
+      callback(quantity_101_2, done_102_0);
+    });
+  }
+
+
+  return {function00};
+}`
+
+export default {vendorPriceMarketConnected, priceMarketEvent1};
