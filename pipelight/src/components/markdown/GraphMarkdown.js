2a5bc6c56f6a36c83187d5798f8e35a00915c477
Loredana <loredana.cirstea@gmail.com>
2020-03-16 19:52:21 +0200

    Pipelight - graph markdown editor - add controlled gapi IO names

--- pipelight/src/components/markdown/GraphMarkdown.js
+++ pipelight/src/components/markdown/GraphMarkdown.js
@@ -3,12 +3,7 @@ import { View } from 'native-base';
 import MDEditor, { commands } from '@uiw/react-md-editor';
 import '@uiw/react-md-editor/dist/markdown-editor.css';
 
-const mdCommands = [
-  commands.bold, commands.italic, commands.strikethrough, commands.hr, commands.title, commands.divider,
-  commands.link, commands.quote, commands.code, commands.divider,
-  commands.unorderedListCommand, commands.orderedListCommand, commands.checkedListCommand, commands.divider,
-  commands.codeEdit, commands.codeLive, commands.codePreview, commands.divider,
-]
+import ControlledInput from './ControlledInput.js';
 
 export default class GraphMarkdown extends Component {
   constructor(props) {
@@ -16,14 +11,48 @@ export default class GraphMarkdown extends Component {
 
     this.state = {
       markdown: props.value,
+      controlledInput: null,
     }
 
+    this.onSelect = this.onSelect.bind(this);
+
     const self = this;
+
+    this.addControlledInput = {
+      name: 'addControlledInput',
+      keyCommand: 'addControlledInput',
+      buttonProps: { 'aria-label': 'Add input/output' },
+      icon: (
+        <svg height="12" width="12" id="svg839" enable-background="new 0 0 512 512" viewBox="0 0 12 12" y="0px" x="0px" >
+          <defs id="defs843" />
+          <path
+             fill="currentColor"
+             id="path833"
+             d="M 3.8375356,4.8588494 2.996627,5.6995828 5.9999218,8.7029305 9.003234,5.6995828 8.1623254,4.8588494 6.6005633,6.4206119 V 0.59406937 H 5.3993152 V 6.3903703 Z M 11.405922,10.204683 V 1.7953873 c 0,-0.6608263 -0.540596,-1.20124793 -1.201248,-1.20124793 H 7.8020036 V 1.7953873 H 10.204587 V 10.204683 H 1.7952924 V 1.7953873 H 4.1979799 V 0.59413937 H 1.7953274 c -0.660828,0 -1.20124904,0.54059663 -1.20124904,1.20124793 v 8.4092957 c 0,0.660826 0.54059604,1.201248 1.20124904,1.201248 h 8.4092946 c 0.660826,0 1.201248,-0.540597 1.201248,-1.201248 z"
+          />
+        </svg>
+      ),
+      execute: (state, api) => {
+        const { controlledInput } = this.state;
+        if (!controlledInput) return;
+
+        let addition = `**[${controlledInput}]**`;
+        let modifyText = `${state.selectedText || ''}${addition}`;
+        api.replaceSelection(modifyText);
+      },
+    };
+
+    this.mdCommands = [
+      commands.bold, commands.italic, commands.strikethrough, commands.hr, commands.title, commands.divider,
+      commands.link, commands.quote, commands.code, commands.divider,
+      commands.unorderedListCommand, commands.orderedListCommand, commands.checkedListCommand, commands.divider,
+      commands.codeEdit, commands.codeLive, commands.codePreview, commands.divider,
+      this.addControlledInput,
+    ]
 
     this.renderers = {
       linkReference: ({ children, language, value }) => {
         const { gapi, gapiValues } = self.props;
-        console.log('self', gapi, gapiValues);
         const linkrefvalue = children[0].props.value;
 
         if (!gapiValues) return children;
@@ -64,6 +93,7 @@ export default class GraphMarkdown extends Component {
     }
 
     const { gapi, gapiValues } = this.props;
+
     if (gapiValues && prevProps.gapiValues !== gapiValues) {
       let value = markdown || this.props.value;
       const gapiValuesParsed = JSON.parse(gapiValues);
@@ -88,25 +118,36 @@ export default class GraphMarkdown extends Component {
     }
   }
 
+  onSelect(iotype, controlledInput) {
+    this.setState({ controlledInput });
+  }
+
   render() {
     const { props } = this;
-    const { styles={}, preview, onChange } = props;
+    const { styles={}, preview, onChange, gapi } = props;
     const { markdown } = this.state;
 
-    let commands = mdCommands;
+    let commands = this.mdCommands;
     if (preview === 'preview') {
       commands = [];
     }
 
     return (
       <View style={styles}>
-      <MDEditor
-        value={markdown}
-        height={styles.height}
-        onChange={onChange}
-        commands={commands}
-        previewOptions={{ renderers: this.renderers, includeNodeIndex: true, sourcePos: true, rawSourcePos: true }}
-      />
+        {preview !== 'preview'
+          ? <ControlledInput
+              gapi={gapi}
+              onSelect={this.onSelect}
+            />
+          : <></>
+        }
+        <MDEditor
+          value={markdown}
+          height={styles.height}
+          onChange={onChange}
+          commands={commands}
+          previewOptions={{ renderers: this.renderers, includeNodeIndex: true, sourcePos: true, rawSourcePos: true }}
+        />
       </View>
     )
   }
